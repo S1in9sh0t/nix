2bb9e04761f528de7e0a15bf14d86e17fefc845b
BNC33 <ben.charlesworth@specsavers.com>
2025-10-31 14:41:26 +0000

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,63 +1,85 @@
 { config, pkgs, inputs, ... }:
 
 {
-  home.stateVersion = "25.05";
-  
+  # Import DMS modules at the TOP LEVEL
+  imports = [
+    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
+    # Uncomment next line if you want niri integration:
+    # inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
+  ];
+
   home.username = "benzo";
   home.homeDirectory = "/home/benzo";
+  home.stateVersion = "25.05";
 
   programs.home-manager.enable = true;
   
-  # Enable DankMaterialShell (module already imported in flake.nix)
-  programs.dankMaterialShell.enable = true;
+  # Enable DankMaterialShell
+  programs.dankMaterialShell = {
+    enable = true;
+  };
 
+  # Hyprland configuration via home-manager
   wayland.windowManager.hyprland = {
     enable = true;
-    extraConfig = ''
-      $mod = SUPER
+    
+    # Basic settings
+    settings = {
+      "$mod" = "SUPER";
       
-      # Terminal
-      bind = $mod, Return, exec, ghostty
-      
-      # Window management
-      bind = $mod, Q, killactive
-      bind = $mod SHIFT, M, exit
-      bind = $mod, F, fullscreen
-      bind = $mod, D, togglefloating
-      
-      # Focus movement
-      bind = $mod, left, movefocus, l
-      bind = $mod, right, movefocus, r
-      bind = $mod, up, movefocus, u
-      bind = $mod, down, movefocus, d
-      
-      # Workspaces
-      bind = $mod, 1, workspace, 1
-      bind = $mod, 2, workspace, 2
-      bind = $mod, 3, workspace, 3
-      bind = $mod, 4, workspace, 4
-      bind = $mod, 5, workspace, 5
-      
-      # Move windows to workspaces
-      bind = $mod SHIFT, 1, movetoworkspace, 1
-      bind = $mod SHIFT, 2, movetoworkspace, 2
-      bind = $mod SHIFT, 3, movetoworkspace, 3
-      bind = $mod SHIFT, 4, movetoworkspace, 4
-      bind = $mod SHIFT, 5, movetoworkspace, 5
+      # Keybinds
+      bind = [
+        # Terminal
+        "$mod, Return, exec, kitty"
+        
+        # Window management
+        "$mod, Q, killactive"
+        "$mod SHIFT, M, exit"
+        "$mod, F, fullscreen"
+        "$mod, D, togglefloating"
+        
+        # Focus movement
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+        
+        # Workspaces
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        
+        # Move windows to workspaces
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        
+        # DMS keybinds
+        "$mod, Space, exec, dms ipc call spotlight toggle"
+        "$mod, V, exec, dms ipc call clipboard toggle"
+        "$mod, N, exec, dms ipc call notifications toggle"
+        "$mod, comma, exec, dms ipc call settings toggle"
+        "$mod, C, exec, dms ipc call control-center toggle"
+        "$mod, X, exec, dms ipc call powermenu toggle"
+        "$mod, P, exec, dms ipc call notepad toggle"
+        "$mod, TAB, exec, dms ipc call hypr toggleOverview"
+      ];
       
       # Mouse bindings
-      bindm = $mod, mouse:272, movewindow
-      bindm = $mod, mouse:273, resizewindow
-      
-      # DMS keybinds
-      bind = $mod, Space, exec, dms ipc call spotlight toggle
-      bind = $mod, V, exec, dms ipc call clipboard toggle
-      bind = $mod, N, exec, dms ipc call notifications toggle
-      bind = $mod, comma, exec, dms ipc call settings toggle
-      bind = $mod, C, exec, dms ipc call control-center toggle
-      bind = $mod, X, exec, dms ipc call powermenu toggle
-      bind = $mod, P, exec, dms ipc call notepad toggle
-      bind = $mod, TAB, exec, dms ipc call hypr toggleOverview
-    '';
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+    };
   };
+
+  # Add some basic packages
+  home.packages = with pkgs; [
+    kitty  # or your preferred terminal
+    # Add other packages you need
+  ];
 }
