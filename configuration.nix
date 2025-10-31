ffa4ec253668e4af45073fbc97bfbfa1979ea649
BNC33 <ben.charlesworth@specsavers.com>
2025-10-31 14:19:45 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
@@ -9,41 +9,55 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/London";
 
-  services.displayManager.ly.enable = true;
-  services.xserver = {
-    enable = true;
-    autoRepeatDelay = 200;
-    autoRepeatInterval = 35;
-    windowManager.qtile.enable = true;
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
 
   users.users.benzo = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    description = "Ben";
+    extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      tree
+      
     ];
   };
 
-  programs.firefox.enable = true;
+  # Wayland login manager for Hyprland/DMS
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "Hyprland";
+        user = "benzo";
+      };
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    ghostty
-    git
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "25.05";
-
 }
