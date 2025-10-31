8e782ea8b8666b78682bbad1e0e3d90be64af052
BNC33 <ben.charlesworth@specsavers.com>
2025-10-31 13:38:33 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,20 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dgop = {
+      url = "github:AvengeMedia/dgop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dms-cli = {
+      url = "github:AvengeMedia/danklinux";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dankMaterialShell = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.dgop.follows = "dgop";
+      inputs.dms-cli.follows = "dms-cli";
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }:
