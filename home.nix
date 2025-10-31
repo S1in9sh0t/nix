bc106c3d5b0fb6a9aac89e4de68da780f71aa26c
BNC33 <ben.charlesworth@specsavers.com>
2025-10-31 13:58:56 +0000

    Update home.nix

--- home.nix
+++ home.nix
@@ -2,51 +2,55 @@
 
 {
   home.stateVersion = "25.05";
-
+  
   home.username = "benzo";
   home.homeDirectory = "/home/benzo";
 
   programs.home-manager.enable = true;
-
-  imports = [
-    inputs.dankMaterialShell.homeModules.dankMaterialShell.default
-  ];
-
+  
+  # Enable DankMaterialShell (module already imported in flake.nix)
   programs.dankMaterialShell.enable = true;
 
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
       $mod = SUPER
-
+      
+      # Terminal
       bind = $mod, Return, exec, ghostty
+      
+      # Window management
       bind = $mod, Q, killactive
       bind = $mod SHIFT, M, exit
       bind = $mod, F, fullscreen
       bind = $mod, Space, togglefloating
-
+      
+      # Focus movement
       bind = $mod, left, movefocus, l
       bind = $mod, right, movefocus, r
       bind = $mod, up, movefocus, u
       bind = $mod, down, movefocus, d
-
+      
+      # Workspaces
       bind = $mod, 1, workspace, 1
       bind = $mod, 2, workspace, 2
       bind = $mod, 3, workspace, 3
       bind = $mod, 4, workspace, 4
       bind = $mod, 5, workspace, 5
-
+      
+      # Move windows to workspaces
       bind = $mod SHIFT, 1, movetoworkspace, 1
       bind = $mod SHIFT, 2, movetoworkspace, 2
       bind = $mod SHIFT, 3, movetoworkspace, 3
       bind = $mod SHIFT, 4, movetoworkspace, 4
       bind = $mod SHIFT, 5, movetoworkspace, 5
-
+      
+      # Mouse bindings
       bindm = $mod, mouse:272, movewindow
       bindm = $mod, mouse:273, resizewindow
-
+      
       # DMS keybinds
-      bind = $mod, Space, exec, dms ipc call spotlight toggle
+      bind = $mod, D, exec, dms ipc call spotlight toggle
       bind = $mod, V, exec, dms ipc call clipboard toggle
       bind = $mod, N, exec, dms ipc call notifications toggle
       bind = $mod, comma, exec, dms ipc call settings toggle
