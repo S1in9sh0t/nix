917deffb12596b148fc86ac5acbafef53354ef25
BNC33 <ben.charlesworth@specsavers.com>
2025-10-31 14:40:55 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,47 +1,56 @@
 {
-  description = "NixOS configuration with Home Manager";
+  description = "NixOS configuration with Home Manager and DankMaterialShell";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
     dgop = {
       url = "github:AvengeMedia/dgop";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
     dms-cli = {
       url = "github:AvengeMedia/danklinux";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.dgop.follows = "dgop";
       inputs.dms-cli.follows = "dms-cli";
     };
+    
+    # Optional: add niri if you want niri integration
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, dankMaterialShell, ... }:
-  {
+  outputs = inputs@{ nixpkgs, home-manager, dankMaterialShell, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
       
       modules = [
         ./configuration.nix
-        home-manager.nixosModules.home-manager
+        
+        inputs.home-manager.nixosModules.home-manager
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
+          _module.args = { inherit inputs; };
           
-          home-manager.users.benzo = {
-            imports = [
-              inputs.dankMaterialShell.homeModules.dankMaterialShell.default
-              ./home.nix
-            ];
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "backup";
+            extraSpecialArgs = { inherit inputs; };
+            
+            users.benzo = import ./home.nix;
           };
         }
       ];
